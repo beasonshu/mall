5d5deb58e56bc5e3d123bc75a7024e8dd786dff0
shuxinghu <beasonshu@sina.com>
2019-09-02 18:04:51 +0800

    开发

--- litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxIndexController.java
+++ litemall-wx-api/src/main/java/org/linlinjava/litemall/wx/web/WxIndexController.java
@@ -3,6 +3,8 @@ package org.linlinjava.litemall.wx.web;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.linlinjava.litemall.core.util.ResponseUtil;
+import org.linlinjava.litemall.db.service.LitemallAdService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/wx/index")
 public class WxIndexController {
     private final Log logger = LogFactory.getLog(WxIndexController.class);
+    @Autowired
+    private LitemallAdService adService;
+
 
     /**
      * 测试数据

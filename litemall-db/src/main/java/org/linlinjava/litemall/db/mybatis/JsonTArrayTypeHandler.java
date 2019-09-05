c7c78461f6a7738f2bc43c6862fc786c3253c40e
shuxinghu <beasonshu@sina.com>
2019-09-05 08:02:08 +0800

    add

---
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/mybatis/JsonTArrayTypeHandler.java
@@ -0,0 +1,61 @@
+package org.linlinjava.litemall.db.mybatis;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+   <columnOverride column="ids" javaType="java.lang.Integer[]" typeHandler="JsonIntegerArrayTypeHandler"/>
+ */
+public class JsonTArrayTypeHandler<T> extends BaseTypeHandler<T[]> {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, T[] parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, toJson(parameter));
+    }
+
+    @Override
+    public T[] getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return this.toObject(rs.getString(columnName));
+    }
+
+
+
+    @Override
+    public T[] getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return this.toObject(rs.getString(columnIndex));
+    }
+
+    @Override
+    public T[] getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        return this.toObject(cs.getString(columnIndex));
+    }
+
+    private String toJson(T[] params) {
+        try {
+            return mapper.writeValueAsString(params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "[]";
+    }
+
+    private T[] toObject(String content) {
+        if (content != null && !content.isEmpty()) {
+            try {
+
+                return mapper.readValue(content, T[].class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return null;
+        }
+    }
+}

09514d96889a0153d565571c2b3d7662bb52b307
shuxinghu <beasonshu@sina.com>
2019-09-05 11:00:57 +0800

    类型更换

--- litemall-db/src/main/java/org/linlinjava/litemall/db/mybatis/JsonTArrayTypeHandler.java
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/mybatis/JsonTArrayTypeHandler.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.ParameterizedType;
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -49,8 +51,9 @@ public class JsonTArrayTypeHandler<T> extends BaseTypeHandler<T[]> {
     private T[] toObject(String content) {
         if (content != null && !content.isEmpty()) {
             try {
+                Class<T> tClass = (Class<T>)((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0];
 
-                return mapper.readValue(content, T[].class);
+                return (T[]) mapper.readValue(content, Array.newInstance(tClass, 0).getClass());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }

b27c6c140b67490b0f9f40f6a6350ccaa5ac739e
shuxinghu <beasonshu@sina.com>
2019-09-06 15:49:47 +0800

    增加菜单表

--- litemall-db/src/main/java/org/linlinjava/litemall/db/service/DiningHallService.java
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/service/DiningHallService.java
@@ -1,7 +1,20 @@
 package org.linlinjava.litemall.db.service;
 
+import org.linlinjava.litemall.db.dao.DiningHallMapper;
+import org.linlinjava.litemall.db.domain.DiningHall;
+import org.linlinjava.litemall.db.domain.DiningHallExample;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class DiningHallService {
+    @Resource
+    private DiningHallMapper diningHallMapper;
+    public List<DiningHall> queryByUid(Integer uid) {
+        DiningHallExample example = new DiningHallExample();
+        example.or();
+        return diningHallMapper.selectByExample(example);
+    }
 }
